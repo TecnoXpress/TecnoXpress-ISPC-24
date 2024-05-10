0f04c25600e428f45e1e364b0f6c5624b3c540df
valentachini <valentinatachini@gmail.com>
2024-05-10 20:04:08 -0300

    se agrego home, bootstrap 5.2.3, y la carpeta media en angular

--- Front-End24/TecnoXpress/src/app/app.component.ts
+++ Front-End24/TecnoXpress/src/app/app.component.ts
@@ -2,13 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from '../app/pages/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,FooterComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet,
+            HomeComponent,
+            HeaderComponent,
+            FooterComponent],
+            templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'TecnoXpress';
--- Front-End24/TecnoXpress/src/app/pages/home/home.component.ts
+++ Front-End24/TecnoXpress/src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
 
