4cdd2f053e35a1ec0b2c35cebe565eb8427a7857
PaoTes <andreaptesta@gmail.com>
2024-05-13 19:41:21 -0300

    form contacto reactivo

--- Front-End24/TecnoXpress/src/app/pages/contact/contact.component.ts
+++ Front-End24/TecnoXpress/src/app/pages/contact/contact.component.ts
@@ -1,12 +1,47 @@
+import { CommonModule, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder,FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrl: './contact.component.css',
+  imports: [CommonModule, ReactiveFormsModule,NgIf],
+
 })
 export class ContactComponent {
 
+  contactForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder)  
+{
+    this.contactForm = this.formBuilder.group(
+      {
+      email: ['', [Validators.required, Validators.email]],
+      nombre: ['', [Validators.required, Validators.minLength(4)]],
+      mensaje: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(500)]],
+    }
+  );
+  
+}
+get Email() {
+  return this.contactForm.get("email");
+     
+}
+
+get Nombre() {
+  return this.contactForm.get("nombre");
+     
+}
+get Mensaje() {
+  return this.contactForm.get("mensaje");
+     
+}
+  onEnviar(event:Event) {
+    
+      console.log(this.contactForm.value);
+    
+}
+  
 }
