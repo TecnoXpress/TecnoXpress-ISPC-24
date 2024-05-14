6def204fcb19af801887e2c973bf508ffd3a7397
CarolinaV22 <kariito.valles@gmail.com>
2024-05-13 22:45:17 -0300

    se agregan componente registro e inicio de sesion

--- Front-End24/TecnoXpress/src/app/pages/iniciar-sesion/iniciar-sesion.component.ts
+++ Front-End24/TecnoXpress/src/app/pages/iniciar-sesion/iniciar-sesion.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-iniciar-sesion',
@@ -7,6 +8,24 @@ import { Component } from '@angular/core';
   templateUrl: './iniciar-sesion.component.html',
   styleUrl: './iniciar-sesion.component.css'
 })
-export class IniciarSesionComponent {
 
-}
+export class IniciarSesionComponent implements OnInit {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      usuario: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(20)]],
+      contraseña: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(15)]]
+    });
+  }
+
+  submitForm() {
+    if (this.loginForm.valid) {
+      console.log("Formulario enviado");
+    } else {
+      console.log("El formulario no se envió.");
+      alert("El formulario no se envió porque hay errores de validación.");
+    }
+  } }
