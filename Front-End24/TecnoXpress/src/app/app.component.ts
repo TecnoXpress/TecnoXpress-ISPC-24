4c3edc3ac89282cf5cfee92fe3e86147d8acd739
valentachini <valentinatachini@gmail.com>
2024-05-23 20:26:18 -0300

    actualizacion rama (se fusiono rama componentes con rama valentachini)

--- Front-End24/TecnoXpress/src/app/app.component.ts
+++ Front-End24/TecnoXpress/src/app/app.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from '../app/pages/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    HeaderComponent,
-    FooterComponent],
+  imports: [RouterOutlet,
+            HomeComponent,
+            HeaderComponent,
+            FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
