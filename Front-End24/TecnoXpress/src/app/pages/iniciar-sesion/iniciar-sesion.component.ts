06d4c92d9d5a693ee3622d256dc01d6a117f29d1
CarolinaV22 <kariito.valles@gmail.com>
2024-05-24 02:47:26 -0300

    formularios reactivos de registro e inicio de sesion

--- Front-End24/TecnoXpress/src/app/pages/iniciar-sesion/iniciar-sesion.component.ts
+++ Front-End24/TecnoXpress/src/app/pages/iniciar-sesion/iniciar-sesion.component.ts
@@ -1,33 +1,40 @@
 import { CommonModule, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+  
+  @Component({
+    selector: 'app-iniciar-sesion',
+    standalone: true,
+    templateUrl: './iniciar-sesion.component.html',
+    styleUrls: ['./iniciar-sesion.component.css'],
+    imports: [CommonModule, ReactiveFormsModule, NgIf]
+  })
+  export class IniciarSesionComponent {
 
-@Component({
-  selector: 'app-iniciar-sesion',
-  standalone: true,
-  templateUrl: './iniciar-sesion.component.html',
-  styleUrls: ['./iniciar-sesion.component.css'],
-  imports: [CommonModule, ReactiveFormsModule,NgIf],
-})
-
-export class IniciarSesionComponent  {
-  loginForm!: FormGroup;
-
-  constructor(private fb: FormBuilder) { }
-
-  ngOnInit(): void {
-    this.loginForm = this.fb.group({
-      usuario: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(20)]],
-      contraseña: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(15)]]
-    });
-  }
-
-  submitForm() {
-    if (this.loginForm.valid) {
-      console.log("Formulario enviado");
-    } else {
-      console.log("El formulario no se envió.");
-      alert("El formulario no se envió porque hay errores de validación.");
+    loginForm: FormGroup; 
+  
+    constructor(
+      private formBuilder: FormBuilder,  
+      private router: Router
+    ) {
+      this.loginForm = this.formBuilder.group({ 
+        usuario: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(20)]],
+        contraseña: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(15)]]
+      });
     }
-  }
-}
+  
+    submitForm(): void {
+      if (this.loginForm.valid) {
+        console.log("Enviando al servidor...");
+        //this.authService.login(this.loginForm.value).subscribe(
+          //() => {
+            console.log("Inicio de sesión exitoso.");
+            this.router.navigate(['/dashboard']); 
+          }
+          (error: any) => {
+            console.error("Error al iniciar sesión:", error);
+          }
+      } //else {
+        //this.loginForm.markAllAsTouched();
+      }
