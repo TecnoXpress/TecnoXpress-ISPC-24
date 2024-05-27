de6c7127489158f578e4f0de7c3d29ba60d2839f
CarolinaV22 <kariito.valles@gmail.com>
2024-05-26 22:56:29 -0300

    Correccion errores de Registro e iniciar sesion

--- Front-End24/TecnoXpress/src/app/pages/registrar/registrar.component.ts
+++ Front-End24/TecnoXpress/src/app/pages/registrar/registrar.component.ts
@@ -12,48 +12,95 @@ import { Router } from '@angular/router';
 })
 export class RegistroComponent {
 
-  registerForm: FormGroup; 
+  registerForm!: FormGroup; 
 
   constructor(
-    private formBuilder: FormBuilder, 
-    private router: Router
-  ) {
-    this.registerForm = this.formBuilder.group({
-      nombre: ['', Validators.required],
-      apellido: ['', Validators.required],
+    private formBuilder: FormBuilder) 
+  {
+    this.registerForm = this.formBuilder.group(
+     {
+      nombre: ['', [Validators.required, Validators.minLength(4)]],
+      apellido: ['', [Validators.required, Validators.minLength(4)]],
       email: ['', [Validators.required, Validators.email]],
-      celular: ['', Validators.required], 
-      dni: ['', Validators.required], 
+      telefono: ['', [Validators.required, Validators.minLength(11), Validators.maxLength(20)]], 
+      dni: ['', [Validators.required, Validators.minLength(7), Validators.maxLength(8)]], 
       fechaNacimiento: ['', Validators.required], 
-      password: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(11)]],
       confirmPassword: ['', Validators.required]
     }, { validator: this.passwordMatchValidator }); 
   }
 
-  passwordMatchValidator(group: FormGroup): { passwordMismatch: boolean } | null {
-    const passwordControl = group.get('password');
-    const confirmPasswordControl = group.get('confirmPassword');
+ 
 
-    if (passwordControl && confirmPasswordControl) { 
-        const password = passwordControl.value;
-        const confirmPassword = confirmPasswordControl.value;
-        return password === confirmPassword ? null : { passwordMismatch: true };
-    } else {
-        return null; 
+  // onSubmit(): void { 
+  //   if (this.registerForm.valid) {
+  //     console.log("Enviando al servidor...");
+  //     //this.authService.register(this.registerForm.value).subscribe( //
+  //      // () => {
+  //         console.log("Registro exitoso.");
+  //         this.router.navigate(['/iniciar-sesion']); 
+  //       }
+  //       (        error: any) => {
+  //         console.error("Error al registrar:", error);
+  //       }
+  //   } //else {
+  //     //this.registerForm.markAllAsTouched();
+    
+    get Email() {
+      return this.registerForm.get("email");
+         
     }
+    
+    get Nombre() {
+      return this.registerForm.get("nombre");
+         
+    }
+
+    get Apellido() {
+      return this.registerForm.get("apellido");
+         
+    }
+    
+    get Telefono() {
+      return this.registerForm.get("telefono");
+         
+    }
+
+    get Dni() {
+      return this.registerForm.get("dni");
+         
+    }
+
+    get FechaNacimiento() {
+      return this.registerForm.get("fechaNacimiento");
+         
+    }
+
+    get Password() {
+      return this.registerForm.get("password");
+         
+    }
+    get ConfirmPassword() {
+      return this.registerForm.get("confirmPassword");
+         
+    }
+
+    passwordMatchValidator(group: FormGroup): { passwordMismatch: boolean } | null {
+      const passwordControl = group.get('password');
+      const confirmPasswordControl = group.get('confirmPassword');
+  
+      if (passwordControl && confirmPasswordControl) { 
+          const password = passwordControl.value;
+          const confirmPassword = confirmPasswordControl.value;
+          return password === confirmPassword ? null : { passwordMismatch: true };
+      } else {
+          return null; 
+      }
+  }
+    onEnviar(event:Event) {
+    
+      console.log(this.registerForm.value);
+    
 }
 
-  onSubmit(): void { 
-    if (this.registerForm.valid) {
-      console.log("Enviando al servidor...");
-      //this.authService.register(this.registerForm.value).subscribe( //
-       // () => {
-          console.log("Registro exitoso.");
-          this.router.navigate(['/iniciar-sesion']); 
-        }
-        (        error: any) => {
-          console.error("Error al registrar:", error);
-        }
-    } //else {
-      //this.registerForm.markAllAsTouched();
-    }
+  }
