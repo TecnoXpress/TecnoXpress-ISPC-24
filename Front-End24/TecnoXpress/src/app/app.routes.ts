a3a47bf65c26d80db0412ef03ce64d16e4f079be
TauTech99 <taoufik.saidi@estudiantes.unahur.edu.ar>
2024-06-02 13:05:23 -0300

    Con Carolina hicimos actualizaciones y emplementación de formularios reactivos, peticiones HTTP con patrón MVC en Angular, conexión con backend usando API de prueba, Guards para rutas protegidas y services para usuario y autenticación.

--- Front-End24/TecnoXpress/src/app/app.routes.ts
+++ Front-End24/TecnoXpress/src/app/app.routes.ts
@@ -4,40 +4,19 @@ import { HomeComponent } from './pages/home/home.component';
 import { ProductosComponent } from './pages/productos/productos.component';
 import { SomosComponent } from './pages/somos/somos.component';
 import { ContactComponent } from './pages/contact/contact.component';
-import { RegistroComponent } from './pages/registrar/registrar.component';
-import { IniciarSesionComponent } from './pages/iniciar-sesion/iniciar-sesion.component';
+import { RegistrarComponent } from './pages/auth/registrar/registrar.component';
+import { IniciarSesionComponent } from './pages/auth/iniciar-sesion/iniciar-sesion.component';
 import { CarritoComponent } from './pages/carrito/carrito.component';
-import {RouterModule} from "@angular/router";
+import { AuthGuard } from './pages/guards/auth.guard';
 
 export const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
-  {path:'dashboard',
-  component: DashboardComponent
-  },
-  {path:'somos',
-  component: SomosComponent
-  },
-  {path:'contact',
-  component: ContactComponent
-  },
-  {path:'registrar',
-  component: RegistroComponent
-  },
-  {path:'iniciar-sesion',
-  component: IniciarSesionComponent
-  },
-  {
-    path: 'home',
-    component: HomeComponent
-  },
-  {
-    path: 'productos',
-    component: ProductosComponent
-  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'somos', component: SomosComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'registrar', component: RegistrarComponent },
+  { path: 'iniciar-sesion', component: IniciarSesionComponent },
+  { path: 'productos', component: ProductosComponent },
   { path: 'carrito', component: CarritoComponent },
-
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
 ];
---
+++ Front-End24/TecnoXpress/src/app/pages/auth/registrar/user.model.ts
@@ -0,0 +1,12 @@
+export interface User {
+    id?: string;
+    nombre: string;
+    nombreUsuario: string;
+    apellido: string;
+    email: string;
+    telefono: string;
+    dni: string;
+    fechaNacimiento: string;
+    password: string;
+    confirmPassword: string;
+  }
